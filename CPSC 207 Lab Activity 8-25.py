20cd86e82c86cf1f919c12e45991834e139fcc06
erusch01 <116841757+erusch01@users.noreply.github.com>
2022-12-03 16:05:35 -0500

    Add files via upload

---
+++ CPSC 207 Lab Activity 8-25.py
@@ -0,0 +1,61 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Camryn Hurley
+print("Hello, world")
+Hello, world
+print("'Welcome to CPSC 207")
+'Welcome to CPSC 207
+print("Hellon\World")
+Hellon\World
+print("Hello\nWorld")
+Hello
+World
+print("Hello\nare\nyou?")
+Hello
+are
+you?
+print("Today", "is", "Tuesday")
+Today is Tuesday
+print("Today" + "is" + "Tuesday")
+TodayisTuesday
+print("Hello\n" + "\nWorld!")
+Hello
+
+World!
+print("123\n45\n6")
+123
+45
+6
+1+2*3
+7
+
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+6%2
+0
+1+2*3**4
+163
+print("2+2=", 2+2)
+2+2= 4
+print("2+2=4")
+2+2=4
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
---
+++ CPSC207 Lab Activity 8-25.py
@@ -0,0 +1,64 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Emily Rusch
+print("Hello World")
+Hello World
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today", "is", "Tuesday")
+Today is Tuesday
+print("Today" + "is" + "Tuesday")
+TodayisTuesday
+print("Hello\n" + "\nWorld!")
+Hello
+
+World!
+print("123\n45\n6")
+123
+45
+6
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+652
+652
+6%2
+0
+1+2*3**4
+163
+print("2+2=", 2+2)
+2+2= 4
+print("2+2",2+2)
+2+2 4
+print("2+2=",2=2)
+SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+print("2+2="),2+2)
+SyntaxError: unmatched ')'
+print("50 pounds =",50*0.453592,"Kilograms")
+50 pounds = 22.6796 Kilograms
